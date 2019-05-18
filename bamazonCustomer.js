cf7dbe557b01c75bf9bc52759eda0e33110fcc51
Johnny Ross <johnnytross21@gmail.com>
2019-05-18 19:46:50 -0400

    v1.2 about time!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,71 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+// var express = require('express');
+// var app = express();
+// var sql = require("mssql");
+// var inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
-    host: "localhost",
+// app.get('/', function (req, res) {
+   
+
+
+//     // config for your database
+//     var config = {
+//         user: 'root',
+//         password: 'docker',
+//         server: 'localhost', 
+//         database: 'bamazon_db' 
+//     };
+
+//     // connect to your database
+//     sql.connect(config, res, function (err) {
+    
+//         if (err) console.log(err);
+
+//         // create Request object
+//         var request = new sql.Request();
+
+//         test()
+            
+        
+//     });
+// });
+
+
+// function test(){
+//   app.query("SELECT * FROM products", function(err, results) {
+//     if (err) throw err;
+//     console.log(results);
+//   })
+// }
+
+// var server = app.listen(5000, function () {
+//     console.log('Server is running..');
+// });
+
+
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const express = require('express');
+
+const connection = mysql.createConnection({
+  host: "192.168.99.100",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "docker",
+  database: "bamazon_db"
+});
   
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "",
-    database: "bamazon_db"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-  });
+connection.connect(function(err) {
+  if (err) {
+    console.log("error:" + err.stack)
+  }
+  console.log("DB connected!")
+});
   
 
-  console.log("Logging!");
+
