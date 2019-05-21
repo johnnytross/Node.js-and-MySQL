2841f9eed10adfb687096bdf8dbe80a62f48aa31
Johnny Ross <johnnytross21@gmail.com>
2019-05-21 13:57:18 -0400

    v1.4 making some progress, can update stock inventory in DB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,52 +1,10 @@
-// var express = require('express');
-// var app = express();
-// var sql = require("mssql");
-// var inquirer = require("inquirer");
-
-// app.get('/', function (req, res) {
-   
-
-
-//     // config for your database
-//     var config = {
-//         user: 'root',
-//         password: 'docker',
-//         server: 'localhost', 
-//         database: 'bamazon_db' 
-//     };
-
-//     // connect to your database
-//     sql.connect(config, res, function (err) {
-    
-//         if (err) console.log(err);
-
-//         // create Request object
-//         var request = new sql.Request();
-
-//         test()
-            
-        
-//     });
-// });
-
-
-// function test(){
-//   app.query("SELECT * FROM products", function(err, results) {
-//     if (err) throw err;
-//     console.log(results);
-//   })
-// }
-
-// var server = app.listen(5000, function () {
-//     console.log('Server is running..');
-// });
-
-
+//Dependencies include mysql for the DB, inquirer for prompts, and express
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const express = require('express');
 
 const connection = mysql.createConnection({
+  //Not using local host, this is the IP of a docker machine that is hosting
   host: "192.168.99.100",
 
   // Your port; if not 3306
@@ -57,22 +15,83 @@ const connection = mysql.createConnection({
 
   // Your password
   password: "docker",
+  //DB
   database: "bamazon_db"
 });
   
+//The connection and throw an error if it doesn't connect
 connection.connect(function(err) {
   if (err) {
     console.log("error:" + err.stack)
   }
+  //Let me know if it does connect
   console.log("DB connected!")
+  //Start the first query
+  initialQuery()
 });
 
-connection.query("SELECT * FROM products", function(err, results) {
-  if (err) throw err;
 
-  console.log(results);
 
-});
+function initialQuery(){
+  connection.query("SELECT product_name, price, item_id, stock_quantity FROM products", function(err, results) {
+    if (err) throw err;
+    console.log('----------------------------------------------------')
+    for (i = 0; i < results.length; i++){
+      console.log("ID:"+results[i].item_id + " |", "Product: " + results[i].product_name + " | ", "Price: $" + results[i].price)
+      
+    }
+    console.log('----------------------------------------------------')
+   inquirer
+    .prompt([
+      {
+        name: 'id',
+        type: 'input',
+        message: "Please enter the ID of the product you'd like to buy."
+      },
+      {
+        name: 'units',
+        type: 'input',
+        message: 'How many units would you like to buy?'
+      }
+    ])
+    .then(function(answer){
+      if (results[answer.id - 1].stock_quantity > answer.units){
+
+          let quantity = results[answer.id - 1].stock_quantity - answer.units;
+          // console.log(quantity);
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: quantity
+              },
+              {
+                item_id: answer.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Database update successful");
+              //function could go here
+            }
+          );
+          console.log('----------------------------------------------------')
+          console.log("number of units: " + answer.units)
+          console.log("Your total is $"+(results[answer.id - 1].price)*answer.units)
+          console.log(results[answer.id - 1].stock_quantity - answer.units + " units left in stock.")
+          initialQuery()
+      }
+      else {
+        console.log("Insufficient quantity!");
+        initialQuery()
+      }
+      // get back the stock the user wants to buy console.log(answer.units)
+      // get the ID back from user | console.log(answer.id);
+      // get the stock quantity back, based on which item the user picked | console.log(results[answer.id - 1].stock_quantity)
+    })
+  
+  });
+}
   
 
 
